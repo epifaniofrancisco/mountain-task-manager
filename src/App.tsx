f6c3c7ea3446e4ccfce245e8ba637a52e07d77e6
epifaniofrancisco <epifaniofrancisco03@gmail.com>
2025-10-04 01:13:19 +0100

    feat: add routing and task management components, update configurations and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Tasks from './Tasks'
+
 function App() {
-  return ()
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/tasks" />} />
+      <Route path="/tasks" element={<Tasks />} />
+    </Routes>
+  )
 }
 
 export default App
---
+++ src/Tasks.tsx
@@ -0,0 +1,8 @@
+export default function Tasks() {
+	return (
+		<div>
+			<h1>Lista de Tarefas</h1>
+			{/* conteúdo da página */}
+		</div>
+	);
+}
